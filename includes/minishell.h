abd93c830079c97f0f09cf579bc04bd8d7fdf94d
Baptiste JAMIN <bjamin@e2r2p7.42.fr>
2016-03-20 14:15:41 +0100

    norme .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: bjamin <bjamin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 18:24:29 by bjamin            #+#    #+#             */
-/*   Updated: 2016/03/18 17:34:20 by bjamin           ###   ########.fr       */
+/*   Updated: 2016/03/20 14:15:23 by bjamin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,40 @@
 
 # define UNUSED(x) (void)(x)
 
-typedef int (*t_func)(void *sh, char **cmds);
+typedef int	(*t_func)(void *sh, char **cmds);
 
 typedef struct stat	t_stat;
 
-typedef struct    s_builtin
+typedef struct			s_builtin
 {
-	char      *name;
-	t_func    func;
-}         t_builtin;
+	char				*name;
+	t_func				func;
+}						t_builtin;
 
-
-typedef struct		s_sh
+typedef struct			s_sh
 {
-	int				argc;
-	char			**argv;
-	char			**env;
-	int       last_res;
-	t_builtin builtins[10];
-}					t_sh;
+	int					argc;
+	char				**argv;
+	char				**env;
+	int					last_res;
+	t_builtin			builtins[10];
+}						t_sh;
 
-char	*minishell_get_env(t_sh *sh, char *var);
-char  **minishell_copy_env(char **env);
-int		minishell_count_env(char **env);
-int   minishell_launch_cmd(t_sh *sh, char *cmd, char **args);
-void  minishell_init_builtins(t_sh *sh);
-int   minishell_builtins_exit(void *sh_, char **cmds);
-int   minishell_builtins_pwd(void *sh_, char **cmds);
-int   minishell_builtins_cd(void *sh_, char **cmds);
-int   minishell_builtins_env(void *sh_, char **cmds);
-int   minishell_builtins_setenv(void *sh_, char **cmds);
-int   minishell_builtins_unsetenv(void *sh_, char **cmds);
-int   minishell_is_builtin(t_sh *sh, char **cmds);
-int   minishell_boot_builtin(t_sh *sh, char **cmds);
-void  minishell_errors_is_directory(char *cmd);
-void  minishell_errors_not_found(char *cmd);
-void  minishell_errors_no_file_directory(char *cmd);
+char					*minishell_get_env(t_sh *sh, char *var);
+char					**minishell_copy_env(char **env);
+int						minishell_count_env(char **env);
+int						minishell_launch_cmd(t_sh *sh, char *cmd, char **args);
+void					minishell_init_builtins(t_sh *sh);
+int						minishell_builtins_exit(void *sh_, char **cmds);
+int						minishell_builtins_pwd(void *sh_, char **cmds);
+int						minishell_builtins_cd(void *sh_, char **cmds);
+int						minishell_builtins_env(void *sh_, char **cmds);
+int						minishell_builtins_setenv(void *sh_, char **cmds);
+int						minishell_builtins_unsetenv(void *sh_, char **cmds);
+int						minishell_is_builtin(t_sh *sh, char **cmds);
+int						minishell_boot_builtin(t_sh *sh, char **cmds);
+void					minishell_errors_is_directory(char *cmd);
+void					minishell_errors_not_found(char *cmd);
+void					minishell_errors_no_file_directory(char *cmd);
 
 #endif
