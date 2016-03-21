ae319f92d62259bee276aa0a1825ee6330df95b3
Baptiste JAMIN <bjamin@e2r2p16.42.fr>
2016-03-21 17:41:22 +0100

    fixes

--- srcs/builtins/minishell_builtins_cd.c
+++ srcs/builtins/minishell_builtins_cd.c
@@ -12,19 +12,20 @@
 
 #include <minishell.h>
 
-int		minishell_builtins_cd_error_no_file_directory(char *path)
+int		minishell_builtins_cd_error_no_file_directory(char *path, int is_file)
 {
-	ft_putstr_fd("cd: ", 2);
-	ft_putstr_fd(path, 2);
-	ft_putstr_fd(": No such file or directory\n", 2);
-	return (2);
-}
-
-int		minishell_builtins_cd_error_not_directory(char *path)
-{
-	ft_putstr_fd("cd: ", 2);
-	ft_putstr_fd(path, 2);
-	ft_putstr_fd(": Not a directory\n", 2);
+	if (!is_file)
+	{
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+	}
+	else
+	{
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": Not a directory\n", 2);
+	}
 	return (2);
 }
 
@@ -51,6 +52,20 @@ void	minishell_builtins_cd_update_path(t_sh *sh, char *old_path, char *path)
 	minishell_builtins_setenv(sh, cmds);
 }
 
+char	*minishell_builtins_cd_manage_home(t_sh *sh, char *path, char **cmds)
+{
+	if ((cmds[1] && ft_strcmp(cmds[1], "~") == 0) ||
+		(cmds[1] && cmds[1][0] == '~' && ft_strchr(cmds[1], '/')))
+	{
+		if (ft_strchr(cmds[1], '/'))
+			return (ft_strfjoin(ft_strjoin(minishell_get_env(sh, "HOME"), "/"),
+				ft_strchr(cmds[1], '/')));
+		else
+			return (ft_strjoin(minishell_get_env(sh, "HOME"), "/"));
+	}
+	return (path);
+}
+
 int		minishell_builtins_cd(void *sh_, char **cmds)
 {
 	t_sh		*sh;
@@ -62,14 +77,14 @@ int		minishell_builtins_cd(void *sh_, char **cmds)
 	sh = (t_sh *)sh_;
 	if (cmds[1] && ft_strcmp(cmds[1], "-") == 0)
 		cmds[1] = minishell_get_env(sh, "OLDPWD");
-	path = (cmds[1] && ft_strcmp(cmds[1], "~") != 0) ? cmds[1] :
-	ft_strdup(minishell_get_env(sh, "HOME"));
+	path = cmds[1];
+	path = minishell_builtins_cd_manage_home(sh, path, cmds);
 	if (!path)
 		path = ".";
 	if (lstat(path, &stat_) == -1)
-		return (minishell_builtins_cd_error_no_file_directory(path));
+		return (minishell_builtins_cd_error_no_file_directory(path, 0));
 	if (!S_ISDIR(stat_.st_mode))
-		return (minishell_builtins_cd_error_not_directory(path));
+		return (minishell_builtins_cd_error_no_file_directory(path, 1));
 	getcwd(old_path, 256);
 	if (chdir(path) != 0)
 		return (minishell_builtins_cd_error_permission(path));
