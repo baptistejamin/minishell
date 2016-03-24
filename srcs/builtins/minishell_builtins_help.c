d575880f251b399c253e9010ad535ada266ea49d
Baptiste Jamin <baptistejamin@gmail.com>
2016-03-24 21:30:39 +0100

    update help cmds

--- srcs/builtins/minishell_builtins_help.c
+++ srcs/builtins/minishell_builtins_help.c
@@ -18,9 +18,10 @@ int		minishell_builtins_help(void *sh_, char **cmds)
 	UNUSED(cmds);
 	ft_putendl("help:\n");
 	ft_putendl("builtins:");
-	ft_putendl("cd [path]");
+	ft_putendl("cd [-LP] [path]");
+	ft_putendl("pwd [-LP]");
 	ft_putendl("exit [code]");
-	ft_putendl("env [var]");
+	ft_putendl("env [-i] [var=value, ...] [cmd]");
 	ft_putendl("setenv [var] [value]");
 	ft_putendl("unsetenv [var]");
 	return (0);
